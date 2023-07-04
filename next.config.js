a6d96184d374e05b151cdf3004d0ba77dbe0bb92
dontmeway <bbakhadyrov@gmail.com>
2023-07-04 12:20:53 +0500

    edit next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix,
+  basePath,
   env: {
     BUILDER_API_KEY: 'ffbb056c7c114e4080317099b533d26f',
     BUILDER_MODEL: 'page',
